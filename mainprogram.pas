65eaf03e98667cf84622fc0dfe34532678a6b0fe
Daniel T. Borelli <daltomi@aol.com>
2016-07-26 20:30:50 -0300

    Nuevo menu; +ref.

--- mainprogram.pas
+++ mainprogram.pas
@@ -53,6 +53,8 @@ uses
     menuAbout: TMenuItem;
     MenuItem3: TMenuItem;
     MenuItem4: TMenuItem;
+    menuVer: TMenuItem;
+    menuVerInfo: TMenuItem;
     menuQuit: TMenuItem;
     progressBar: TProgressBar;
     StaticText1: TStaticText;
@@ -66,8 +68,10 @@ uses
     procedure FormCreate(Sender: TObject);
     procedure lstBoxDevicesDblClick(Sender: TObject);
     procedure menuAboutClick(Sender: TObject);
+    procedure menuVerClick(Sender: TObject);
     procedure menuQuitClick(Sender: TObject);
     procedure askFormatContinue();
+    procedure menuVerInfoClick(Sender: TObject);
     procedure UdevEvent(AData: PtrInt; AFlags: dword);
     procedure AddItemToDevicesList;
     procedure RemoveItemFromDevicesList;
@@ -97,34 +101,34 @@ implementation
 { TMainForm }
 
 
-procedure TMainForm.btnCloseClick(Sender: TObject);
+procedure TMainForm.btnCloseClick (Sender: TObject);
 begin
      Quit;
 end;
 
 procedure TMainForm.Quit;
 begin
-  RemoveEventhandler(UdevEventEventHandler);
-  UnitializeUdev;
-  m_hash_devices.ForEachCall(@FreeAllHash, nil);
-  m_hash_devices.Destroy;
+  RemoveEventhandler (UdevEventEventHandler);
+  UnitializeUdev ();
+  m_hash_devices.ForEachCall (@FreeAllHash, nil);
+  m_hash_devices.Destroy ();
   Close;
 end;
 
-procedure TMainForm.btnStartClick(Sender: TObject);
+procedure TMainForm.btnStartClick (Sender: TObject);
 begin
-  askFormatContinue;
+  askFormatContinue ();
 end;
 
-procedure TMainForm.FreeAllHash(A : pointer; B : pointer);
+procedure TMainForm.FreeAllHash (A : pointer; B : pointer);
 begin
-   Dispose (PDeviceProperty(A));
+   Dispose (PDeviceProperty (A));
 end;
 
-procedure TMainForm.FillListDevices(A : pointer; B : pointer);
+procedure TMainForm.FillListDevices (A : pointer; B : pointer);
 begin
-   m_device := PDeviceProperty(A);
-   NewListItem;
+   m_device := PDeviceProperty (A);
+   NewListItem ();
 end;
 
 
@@ -132,14 +136,14 @@ end;
 Pointer function callback.
 External Call.
 }
-procedure ListDevicesCallback(); cdecl;
+procedure ListDevicesCallback (); cdecl;
 begin
-     MainForm.m_device := GetDeviceProperty;
-     MainForm.AddItemToDevicesList;
+     MainForm.m_device := GetDeviceProperty ();
+     MainForm.AddItemToDevicesList ();
 end;
 
 
-procedure TMainForm.NewListItem;
+procedure TMainForm.NewListItem ();
 var
    attr : string;
 begin
@@ -148,39 +152,39 @@ begin
      lstBoxDevices.ItemIndex := lstBoxDevices.Count - 1;
 end;
 
-procedure TMainForm.AddItemToDevicesList;
+procedure TMainForm.AddItemToDevicesList ();
 var
    dev_copy : PDeviceProperty;
 begin
-   NewListItem;
+   NewListItem ();
    New (dev_copy);
-   move (m_device^, dev_copy^, sizeof(TDeviceProperty));
+   Move (m_device^, dev_copy^, sizeof (TDeviceProperty));
    m_hash_devices.add (dev_copy^.node_path, dev_copy);
-   if FormInfo.IsVisible then FormInfo.FillInformation (@m_hash_devices, @lstBoxDevices);
+   if FormInfo.IsVisible () then FormInfo.FillInformation (@m_hash_devices, @lstBoxDevices);
 end;
 
 
-procedure TMainForm.RemoveItemFromDevicesList;
+procedure TMainForm.RemoveItemFromDevicesList ();
 var
    dev : PDeviceProperty;
 begin
-   dev := m_hash_devices.Find(m_device^.node_path);
+   dev := m_hash_devices.Find (m_device^.node_path);
    if dev <> nil then
    begin
-     m_hash_devices.Delete  ( m_hash_devices.IndexOf(dev));
+     m_hash_devices.Delete (m_hash_devices.IndexOf (dev));
      Dispose (dev);
    end;
 
-   lstBoxDevices.Items.Clear;
-   m_hash_devices.ForEachCall(@FillListDevices, nil);
-   if FormInfo.IsVisible then FormInfo.FillInformation(@m_hash_devices, @lstBoxDevices);
+   lstBoxDevices.Items.Clear ();
+   m_hash_devices.ForEachCall (@FillListDevices, nil);
+   if FormInfo.IsVisible () then FormInfo.FillInformation (@m_hash_devices, @lstBoxDevices);
 end;
 
 
 {
  Event Handle Callback
 }
-procedure TMainForm.UdevEvent(AData: PtrInt; AFlags: dword);
+procedure TMainForm.UdevEvent (AData: PtrInt; AFlags: dword);
 const
    ADD = 1;      //ref. udev.c
    REMOVE = 2;   //ref. udev.c
@@ -193,24 +197,24 @@ begin
 
   if ret = ADD then
   begin
-    m_device := GetDeviceProperty;
-    AddItemToDevicesList;
+    m_device := GetDeviceProperty ();
+    AddItemToDevicesList ();
   end
   else if ret = REMOVE then
   begin
-    m_device := GetDeviceProperty;
-    RemoveItemFromDevicesList;
+    m_device := GetDeviceProperty ();
+    RemoveItemFromDevicesList ();
   end
 end;
 
 
-procedure TMainForm.FormCreate(Sender: TObject);
+procedure TMainForm.FormCreate (Sender: TObject);
 begin
-  FormInfo := TFormInfo.Create(Application);  // creado explícitamente.
-  m_hash_devices := TFPHashList.Create;
-  InitializeUdev;
-  ListDevices ( @ListDevicesCallback );
-  UdevEventEventHandler := AddEventHandler( GetMonitorEvent, 3, @UdevEvent,0 );
+  FormInfo := TFormInfo.Create (Application);  // creado explícitamente.
+  m_hash_devices := TFPHashList.Create ();
+  InitializeUdev ();
+  ListDevices (@ListDevicesCallback);
+  UdevEventEventHandler := AddEventHandler (GetMonitorEvent, 3, @UdevEvent,0);
 end;
 
 procedure TMainForm.lstBoxDevicesDblClick(Sender: TObject);
@@ -220,24 +224,35 @@ begin
 end;
 
 
-procedure TMainForm.menuAboutClick(Sender: TObject);
+procedure TMainForm.menuAboutClick (Sender: TObject);
 begin
-  aboutForm.ShowModal;
+  aboutForm.ShowModal ();
+end;
+
+procedure TMainForm.menuVerClick (Sender: TObject);
+begin
+  if lstBoxDevices.SelCount  > 0 then
+     menuVerInfo.Enabled := True;
 end;
 
 
-procedure TMainForm.menuQuitClick(Sender: TObject);
+procedure TMainForm.menuQuitClick (Sender: TObject);
 begin
-  Quit;
+  Quit ();
 end;
 
 
-procedure TMainForm.askFormatContinue();
+procedure TMainForm.askFormatContinue ();
 const
  MSG_FORMAT_TITLE = 'Confirmación';
  MSG_FORMAT_BODY  = '¿Desea comenzar con el formato?' + #13#13 + 'Perderá los datos del dispositivo.';
 begin
- MessageDlg(MSG_FORMAT_TITLE, MSG_FORMAT_BODY, mtConfirmation, [mbYes, mbNo], 0) { = mrYes then }
+ MessageDlg (MSG_FORMAT_TITLE, MSG_FORMAT_BODY, mtConfirmation, [mbYes, mbNo], 0) { = mrYes then }
+end;
+
+procedure TMainForm.menuVerInfoClick (Sender: TObject);
+begin
+    lstBoxDevicesDblClick (nil);
 end;
 
 
